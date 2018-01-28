cc1e654484458551ec42975a054d3004d0f5e34a
Brian C Sparks <brian.c.sparks@hp.com>
2018-01-28 06:45:31 +0000

    Moved to mongo for xapi

--- layer67-plugins/xapi/xapi-router.js
+++ layer67-plugins/xapi/xapi-router.js
@@ -11,6 +11,7 @@ const adminsDb                = require('../../lib/admins-db');
 const http                    = require('http');
 const urlLib                  = require('url');
 const redisLib                = require('redis');
+var   MongoClient             = require('mongodb').MongoClient;
 
 const ARGV                    = sg.ARGV();
 const argvGet                 = sg.argvGet;
@@ -20,137 +21,153 @@ const deref                   = sg.deref;
 const unhandled               = unhandledRoutes.unhandled;
 const redisPort               = argvGet(ARGV, 'redis-port')             || 6379;
 const redisHost               = argvGet(ARGV, 'redis-host')             || 'redis';
+var   namespace               = 'layer67';
 
 const redis                   = redisLib.createClient(redisPort, redisHost);
 
+var   bootstrap;
+
 const main = function() {
+  return bootstrap(function(err, db, config_) {
+    const adminsDb = db.db('layer67').collection('admins');
 
-  const ip          = ARGV.ip       || '127.0.0.1';
-  const port        = ARGV.port;
 
-  if (!port) {
-    console.log('Need --port=');
-    process.exit(2);
-  }
+    const ip          = ARGV.ip       || '127.0.0.1';
+    const port        = ARGV.port;
 
-  var serviceIndex = 0;
+    if (!port) {
+      console.log('Need --port=');
+      process.exit(2);
+    }
 
-  const server = http.createServer(function(req, res) {
+    var serviceIndex = 0;
 
-    // We are a long-poll server
-    req.setTimeout(0);
-    res.setTimeout(0);
+    const server = http.createServer(function(req, res) {
 
-    const url   = urlLib.parse(req.url, true);
-    const parts = _.rest(url.pathname.toLowerCase().split('/'));
+      // We are a long-poll server
+      req.setTimeout(0);
+      res.setTimeout(0);
 
-    // ---------- Verify user ----------
+      const url   = urlLib.parse(req.url, true);
+      const parts = _.rest(url.pathname.toLowerCase().split('/'));
 
-    var   service;
-    var   serviceNames = [];
-    return sg.__run([function(next) {
+      // ---------- Verify user ----------
 
-      // A header of X-Client-Verify means nginx was told to check client certs
-      if (req.headers['x-client-verify']) {
-        if (req.headers['x-client-verify'] !== 'SUCCESS') { return sg._403(req, res); }
+      var   service;
+      var   serviceNames = [];
+      return sg.__run([function(next) {
 
-        const serverSigner   = req.headers['x-client-i-dn'];
-        const clientCryptoId = req.headers['x-client-s-dn'];
-        const email          = (utils.parseClientCert(clientCryptoId || '') || {}).CN;
+        // A header of X-Client-Verify means nginx was told to check client certs
+        if (req.headers['x-client-verify']) {
+          var msg = 'verify...';
+          if (req.headers['x-client-verify'] !== 'SUCCESS') { msg+='fail'; console.error(msg); return sg._403(req, res); }
+          msg += 'SUCCESS.';
 
-        console.log(req.headers);
+          const serverSigner   = req.headers['x-client-i-dn'];
+          const clientCryptoId = req.headers['x-client-s-dn'];
+          const email          = (utils.parseClientCert(clientCryptoId || '') || {}).CN;
 
-        if (!serverSigner || !clientCryptoId || !email) {
-          return sg._403(req, res);
+          //console.log(req.headers);
+          console.log(clientCryptoId);
+
+          if (!serverSigner || !clientCryptoId || !email) {
+            msg+='!headers';
+            console.error(msg);
+            return sg._403(req, res);
+          }
+          msg=email+','+msg+',good-headers';
+
+          // Now, look up the email in our DB
+          return adminsDb.find({email:email}).toArray(function(err, result) {
+
+            if (!sg.ok(err, result)) { msg+='unknown'; console.error(msg); return sg._403(req, res); }
+            msg+=',is-in-the-db';
+
+            const role = result[0].role;
+            if (role !== 'admin') { msg+='!admin'; console.error(msg); return sg._403(req, res); }
+            msg+=',Pass.';
+            console.log(msg);
+
+            return next();
+          });
         }
 
-        // Now, look up the email in our DB
-        return adminsDb.get(email, {}, function(err, result) {
-
-          if (!sg.ok(err, result)) { return sg._403(req, res); }
-
-          const role = deref(result, 'Item.rolerole.S');
-          if (role !== 'admin') { return sg._403(req, res); }
-
-          return next();
-        });
-      }
-
-      return next();
-
-    }, function(next) {
-      if (parts.length === 0) {
-        return sg._400(req, res);
-      }
-
-      if (parts.length >= 3) {
-        // Try for the longer service name
-        if (parts[1].match(/(xapi|api)/) && parts[2].match(/v[0-9.]+/)) {
-          if (parts.length >= 4) {
-            serviceNames.push('/'+parts.slice(0, 4).join('/'));
-          }
-          serviceNames.push('/'+parts.slice(0, 3).join('/'));
-        }
-      }
-
-      serviceNames.push('/'+parts[0]);
-
-      // Now, simply see if anyone has registered for any of them
-
-      return sg.__each(serviceNames, function(serviceName, next) {
-        return getServices(serviceName, (err, services) => {
-          if (!sg.ok(err, services)) { return next(); }
-
-          // Are there any services available?
-          if (services.length === 0) { return next(); }
-
-          // If so, pick one
-          if (++serviceIndex >= services.length) {
-            serviceIndex -= services.length;
-            if (serviceIndex >= services.length) {
-              serviceIndex = 0;
-            }
-          }
-
-          service = services[serviceIndex++];
-          return next();
-        });
-      }, function() {
         return next();
+
+      }, function(next) {
+        if (parts.length === 0) {
+          return sg._400(req, res);
+        }
+
+        if (parts.length >= 3) {
+          // Try for the longer service name
+          if (parts[1].match(/(xapi|api)/) && parts[2].match(/v[0-9.]+/)) {
+            if (parts.length >= 4) {
+              serviceNames.push('/'+parts.slice(0, 4).join('/'));
+            }
+            serviceNames.push('/'+parts.slice(0, 3).join('/'));
+          }
+        }
+
+        serviceNames.push('/'+parts[0]);
+
+        // Now, simply see if anyone has registered for any of them
+
+        return sg.__each(serviceNames, function(serviceName, next) {
+          return getServices(serviceName, (err, services) => {
+            if (!sg.ok(err, services)) { return next(); }
+
+            // Are there any services available?
+            if (services.length === 0) { return next(); }
+
+            // If so, pick one
+            if (++serviceIndex >= services.length) {
+              serviceIndex -= services.length;
+              if (serviceIndex >= services.length) {
+                serviceIndex = 0;
+              }
+            }
+
+            service = services[serviceIndex++];
+            return next();
+          });
+        }, function() {
+          return next();
+        });
+
+      }, function(next) {
+        // Check that we have a service
+        if (service) { return next(); }
+
+        // Nope.  Do more?  Just 404.
+        if (!isProd()) {
+          console.log('No services found for: ', serviceNames);
+        }
+
+        return unhandled(req, res);
+
+      }], function done() {
+        // Got one -- Magic nginx potion
+        // location ~* ^/rpxi/GET/(.*) {...}
+        // location ~* ^/rpxissl/GET/(.*) {...}
+
+        // TODO: if we are rpxi-ing ssl, use ssl version
+        const svc     = service.replace(/(http|https):[/][/]/i, '');
+
+        const redir   = sg.normlz(`/rpxi/${req.method.toUpperCase()}/${svc}/${req.url}`);
+
+        console.log('xapi: '+url.pathname+' --> '+redir);
+
+        res.statusCode = 200;
+        res.setHeader('X-Accel-Redirect', redir);
+        res.end();
       });
 
-    }, function(next) {
-      // Check that we have a service
-      if (service) { return next(); }
-
-      // Nope.  Do more?  Just 404.
-      if (!isProd()) {
-        console.log('No services found for: ', serviceNames);
-      }
-
-      return unhandled(req, res);
-
-    }], function done() {
-      // Got one -- Magic nginx potion
-      // location ~* ^/rpxi/GET/(.*) {...}
-      // location ~* ^/rpxissl/GET/(.*) {...}
-
-      // TODO: if we are rpxi-ing ssl, use ssl version
-      const svc     = service.replace(/(http|https):[/][/]/i, '');
-
-      const redir   = sg.normlz(`/rpxi/${req.method.toUpperCase()}/${svc}/${req.url}`);
-
-      console.log('xapi: '+url.pathname+' --> '+redir);
-
-      res.statusCode = 200;
-      res.setHeader('X-Accel-Redirect', redir);
-      res.end();
     });
 
-  });
-
-  server.listen(port, ip, function() {
-    console.log(`Xapi listening on ${ip}:${port}`);
+    server.listen(port, ip, function() {
+      console.log(`Xapi listening on ${ip}:${port}`);
+    });
   });
 };
 
@@ -164,6 +181,26 @@ const maintainTime = function() {
 };
 maintainTime();
 
+bootstrap = function(callback) {
+  const dbAddress = process.env.SERVERASSIST_DB_IP;
+  var   dbUrl     = 'mongodb://10.12.21.229:27017/'+namespace;
+
+  var   db, config = {};
+
+  return sg.__run([function(next) {
+    if (db) { return next(); }
+
+    return MongoClient.connect(dbUrl, function(err, db_) {
+      if (!sg.ok(err, db_)) { return process.exit(2); }
+
+      db = db_;
+      return next();
+    });
+
+  }], function done() {
+    return callback(null, db, config);
+  });
+};
 
 main();
 
